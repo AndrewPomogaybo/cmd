78b2372ac565a16204c8b693925243905fcaafc3
Andrew Pomogaybo <student@247-7>
2023-04-27 09:59:12 +0300

    Реализована команда ls

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +12,39 @@ namespace pomogaybo
     {
         static void Main(string[] args)
         {
+            string query = Console.ReadLine();
+
+            switch (query) 
+            {
+                case "ls":
+                    string dirName = "C:\\";
+                    if (Directory.Exists(dirName))
+                    {
+                        string[] dirs = Directory.GetDirectories(dirName);
+                        foreach (string s in dirs)
+                        {
+                            Console.WriteLine(s);
+                        }
+                        Console.WriteLine();
+                        string[] files = Directory.GetFiles(dirName);
+                        foreach (string s in files)
+                        {
+                            Console.WriteLine(s);
+                        }
+                    }
+                    break;
+                case "--help":
+                    Process process = new Process();
+                    process.StartInfo.FileName = "git";
+                    process.StartInfo.Arguments = "help";
+                    process.StartInfo.UseShellExecute = false;
+                    process.StartInfo.RedirectStandardOutput = true;
+                    process.Start();
+                    query = process.StandardOutput.ReadToEnd();
+                    Console.WriteLine(query);
+                    break;
+            }
+            Console.ReadKey();
         }
     }
 }
